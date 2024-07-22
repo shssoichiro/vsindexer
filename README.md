e81da0e30ed49b7bf43a6cdf498f2fb3cb838616
Josh Holmer <jholmer.in@gmail.com>
2024-07-22 10:26:47 -0400

    Write the rest of the owl

---
+++ README.md
@@ -0,0 +1,3 @@
+Usage: `vsindexer [input_dir]`
+
+Creates index files for all the vapoursynth scripts in a directory, recursively. Open sourced because it might be useful to someone else. This took like 5 minutes to write.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{env::args, ffi::OsStr, process::Command};
+
+use walkdir::WalkDir;
+
 fn main() {
-    println!("Hello, world!");
+    let dir = args().nth(1).expect("Usage: vsindexer [input_dir]");
+
+    for file in WalkDir::new(dir)
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|e| e.path().extension() == Some(OsStr::new("vpy")))
+    {
+        eprintln!("Indexing {}", file.path().to_string_lossy());
+        let _ = Command::new("vspipe").arg("-i").arg(file.path()).status();
+    }
+
+    eprintln!("Done");
 }
