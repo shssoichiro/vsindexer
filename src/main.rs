fa0a55420122bf40ebe0f831be595f0865675cad
Josh Holmer <jholmer.in@gmail.com>
2024-08-17 23:54:19 -0400

    Add multiple args support

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,21 @@ use std::{env::args, ffi::OsStr, process::Command};
 use walkdir::WalkDir;
 
 fn main() {
-    let dir = args().nth(1).expect("Usage: vsindexer [input_dir]");
+    let dirs = args().skip(1).collect::<Vec<_>>();
 
-    for file in WalkDir::new(dir)
-        .into_iter()
-        .filter_map(Result::ok)
-        .filter(|e| e.path().extension() == Some(OsStr::new("vpy")))
-    {
-        eprintln!("Indexing {}", file.path().to_string_lossy());
-        let _ = Command::new("vspipe").arg("-i").arg(file.path()).status();
+    if dirs.is_empty() {
+        panic!("Usage: vsindexer [input_dirs]");
+    }
+
+    for dir in dirs {
+        for file in WalkDir::new(dir)
+            .into_iter()
+            .filter_map(Result::ok)
+            .filter(|e| e.path().extension() == Some(OsStr::new("vpy")))
+        {
+            eprintln!("Indexing {}", file.path().to_string_lossy());
+            let _ = Command::new("vspipe").arg("-i").arg(file.path()).status();
+        }
     }
 
     eprintln!("Done");
